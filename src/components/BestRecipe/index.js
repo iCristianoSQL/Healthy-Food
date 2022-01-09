26c54cf04fd4f15027e8e538d9eb30bc78addbe5
Cristiano <cristianosql02@gmail.com>
2022-01-08 22:56:53 -0300

    feat: criação do componente recipe

---
+++ src/components/BestRecipe/index.js
@@ -0,0 +1,33 @@
+import * as S from "./styles";
+import { TitleAndParagraph, Button } from "..";
+import foodList from "./content";
+
+const BestRecipe = () => {
+  return (
+    <S.Section>
+      <S.TitleDiv>
+        <TitleAndParagraph
+          contentP="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+          contentH="Our Best Recipes"
+        />
+      </S.TitleDiv>
+      <S.ArticleContent>
+        {foodList.map(({ image, title }) => (
+          <S.GeneralContent>
+            <S.ImageDiv>
+              <S.Image src={image} />
+            </S.ImageDiv>
+            <S.ContentDiv>
+              <TitleAndParagraph contentH={title} />
+              <S.ButtonDiv>
+                <Button content="See Recipe" />
+              </S.ButtonDiv>
+            </S.ContentDiv>
+          </S.GeneralContent>
+        ))}
+      </S.ArticleContent>
+    </S.Section>
+  );
+};
+
+export default BestRecipe;
---
+++ src/components/BestRecipe/styles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitleDiv = styled.div`
+  margin: 25px 0px 25px 0px;
+  gap: 15px;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+export const ArticleContent = styled.article`
+  width: 100%;
+  padding: 0 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5px;
+`;
+
+export const GeneralContent = styled.div`
+  width: 45%;
+  height: 30%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const ImageDiv = styled.div`
+  width: 40%;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
+
+export const ContentDiv = styled.div`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1:nth-child(1) {
+    font-size: 24px;
+    width: 200px;
+    margin-bottom: 15px;
+  }
+`;
+
+export const ButtonDiv = styled.button`
+  width: 48%;
+  height: 17%;
+  border: none;
+  border-radius: 50px;
+
+  button:nth-child(1) {
+    border: none;
+    border-radius: 5px;
+    font-family: Mulish;
+    background-color: #badc58;
+    color: white;
+    cursor: pointer;
+  }
+`;
