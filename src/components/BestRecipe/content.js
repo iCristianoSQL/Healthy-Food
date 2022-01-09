baf589a7420b8a142e5d48329fe2e0fc3758c499
Cristiano <cristianosql02@gmail.com>
2022-01-08 22:56:17 -0300

    feat: construindo a estrutura da lista do recipe

---
+++ src/components/BestRecipe/content.js
@@ -0,0 +1,26 @@
+/* Assets */
+import BroccoliSalad from "../../assets/svg/BroccoliSalad.svg";
+import ClassicBurger from "../../assets/svg/ClassicBurger.svg";
+import PotatoSalad from "../../assets/svg/PotatoSalad.svg";
+import CherryCobbler from "../../assets/svg/CherryCobbler.svg";
+
+const foodList = [
+  {
+    image: BroccoliSalad,
+    title: "Broccoli Salad with Bacon",
+  },
+  {
+    image: ClassicBurger,
+    title: "Classic Beef Burgers",
+  },
+  {
+    image: PotatoSalad,
+    title: "Classic Potato Salad",
+  },
+  {
+    image: CherryCobbler,
+    title: "Cherry Cobbler on the Grill",
+  },
+];
+
+export default foodList;
