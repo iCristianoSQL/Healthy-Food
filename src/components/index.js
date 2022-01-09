efb940ebd5d60dcc7f929935c817c7ef0ea582a6
Cristiano <cristianosql02@gmail.com>
2022-01-08 22:55:35 -0300

    feat: adicionando novas importações

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,14 @@ import Button from "./Button";
 import Header from "./Header";
 import Nutrition from "./Nutrition";
 import SuggestionCard from "./SuggestionCard";
+import BestRecipe from "./BestRecipe";
+import TitleAndParagraph from "./TitleAndParagraph";
 
-export { Button, Header, Nutrition, SuggestionCard };
+export {
+  Button,
+  Header,
+  Nutrition,
+  SuggestionCard,
+  BestRecipe,
+  TitleAndParagraph,
+};
