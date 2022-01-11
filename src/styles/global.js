29d98281f2c146b5672106e99e5fb0e4072ce7d5
Cristiano <cristianosql02@gmail.com>
2022-01-11 15:40:06 -0300

    fix: removendo o bug de scroll lateral da pagina

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body, html {
-    width: 100vw;
-    height: 100vh;
+    overflow-x: hidden;
   }
 
   * {
