ae04466a509717ed558db09a6d1199c4e3f9ea93
Cristiano <cristianosql02@gmail.com>
2022-01-11 15:37:38 -0300

    styles: estilizando o carousel

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -20,3 +20,33 @@ export const Image = styled.img`
   background-color: red;
   border-radius: 15px;
 `;
+
+export const Figure = styled.figure``;
+
+export const Legend = styled.figcaption`
+  color: #1d164d;
+  font-family: Montserrat-Bold;
+  font-size: 25px;
+  text-align: start;
+  margin-top: 10px;
+`;
+
+export const Profile = styled.div`
+  width: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: flex-start;
+  gap: 10px;
+  padding-left: 5px;
+  margin-top: 10px;
+`;
+
+export const ProfileImage = styled.img``;
+
+export const Name = styled.p`
+  display: flex;
+  align-items: center;
+  font-family: Mulish;
+  color: #9e9baf;
+  font-size: 16px;
+`;
---
+++ src/components/MemberShip/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  margin-top: 8%;
+  width: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  h2:nth-child(1) {
+    font-size: 38px;
+    width: 75%;
+  }
+  input:nth-child(1) {
+    width: 65%;
+    height: 55px;
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+`;
