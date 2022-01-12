43e4a362667aef47ea5006ef028c8816703b67a3
Cristiano <cristianosql02@gmail.com>
2022-01-12 15:31:38 -0300

    fix: refazendo todo o carousel, a fim de melhorar a responsividade e deixar o mais fiel ao designer

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,45 +1,67 @@
+import { useRef } from "react";
 import * as S from "./styles";
 import listCarousel from "./content";
 import { TitleAndParagraph } from "..";
 
-const MainCarousel = () => {
-  const breakPoint = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2.5 },
-    { width: 850, itemsToShow: 3.5 },
-    { width: 1150, itemsToShow: 3.5 },
-    { width: 1450, itemsToShow: 3.5 },
-    { width: 1750, itemsToShow: 3.5 },
-  ];
+const CardBlog = ({ image, figCaption, profileImage, name }) => {
   return (
-    <S.Section>
-      <S.TextDiv>
-        <TitleAndParagraph
-          contentH={"Read Our Blog"}
-          contentP={
-            "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
-          }
-        />
-      </S.TextDiv>
-      <S.Containercarousel
-        breakPoints={breakPoint}
-        pagination={false}
-        itemsToScroll={1}
-      >
-        {listCarousel.map(({ image, id, figCaption, profileImage, name }) => (
-          <S.Figure key={id}>
-            <S.Image src={image} />
-            <S.FlexContainer>
-              <S.Legend>{figCaption}</S.Legend>
-              <S.Profile>
-                <S.ProfileImage src={profileImage} />
-                <S.Name>{name}</S.Name>
-              </S.Profile>
-            </S.FlexContainer>
-          </S.Figure>
-        ))}
-      </S.Containercarousel>
-    </S.Section>
+    <S.ContainerCardBlog>
+      <S.CardBlog>
+        <S.ContainerCardBlogImage>
+          <S.Image src={image} alt="imagem" />
+        </S.ContainerCardBlogImage>
+        <S.ContainerCardContent>
+          <S.TitleCard>{figCaption}</S.TitleCard>
+          <S.ContainerCardAuthor>
+            <S.AuthorImage src={profileImage} alt="Imagem do autor" />
+            <S.Author>{name}</S.Author>
+          </S.ContainerCardAuthor>
+        </S.ContainerCardContent>
+      </S.CardBlog>
+    </S.ContainerCardBlog>
+  );
+};
+
+const MainCarousel = () => {
+  const carousel = useRef(null);
+  const contentP =
+    "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts";
+
+  const handleLeftClick = () => {
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = () => {
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  return (
+    <S.ContainerReadBlog>
+      <TitleAndParagraph contentH="Read Our Blog" contentP={contentP} />
+      <S.ScrollCardsBlog>
+        <S.ContainerCards ref={carousel}>
+          {listCarousel.map(({ id, image, figCaption, profileImage, name }) => {
+            return (
+              <CardBlog
+                key={id}
+                image={image}
+                figCaption={figCaption}
+                profileImage={profileImage}
+                name={name}
+              />
+            );
+          })}
+        </S.ContainerCards>
+
+        <S.ArrowLeft onClick={handleLeftClick}>
+          <S.SVGArrowLeft color="#8ed062" />
+        </S.ArrowLeft>
+
+        <S.ArrowRight onClick={handleRightClick}>
+          <S.SVGArrowRight color="#8ed062" />
+        </S.ArrowRight>
+      </S.ScrollCardsBlog>
+    </S.ContainerReadBlog>
   );
 };
 
--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -1,66 +1,131 @@
 import styled from "styled-components";
-import Carousel from "react-elastic-carousel";
+import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
 
-export const Section = styled.section`
+// Component Card
+export const ContainerCardBlog = styled.div`
   width: 100%;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  max-width: 21.75rem;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 0 1rem;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  flex: none;
+
+  @media screen and (max-width: 768px) {
+    max-width: 20rem;
+    margin: 0 1rem 0 0;
+  }
 `;
 
-export const TextDiv = styled.div`
-  width: 35%;
+export const CardBlog = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin: 25px 0px;
+  justify-content: center;
+`;
+
+export const ContainerCardBlogImage = styled.div`
+  display: flex;
 `;
 
 export const Image = styled.img``;
 
-export const Figure = styled.div`
-  margin: 0 1.5rem;
-  box-shadow: 0 7px 29px 0 hsl(240deg 5% 41% / 20%);
-`;
+export const ContainerCardContent = styled.div`
+  padding: 1.7rem 1.7rem 1.7rem 1.2rem;
 
-export const Legend = styled.h3`
-  color: #1d164d;
-  font-family: Montserrat-Bold;
-  font-size: 25px;
-  text-align: start;
-  margin-top: 10px;
-`;
-
-export const Profile = styled.div`
-  width: 100%;
-  display: flex;
-  align-content: center;
-  justify-content: flex-start;
-  gap: 10px;
-  padding-left: 5px;
-  margin-top: 10px;
-`;
-
-export const ProfileImage = styled.img``;
-
-export const Name = styled.p`
-  display: flex;
-  align-items: center;
-  font-family: Mulish;
-  color: #9e9baf;
-  font-size: 16px;
-`;
-
-export const Containercarousel = styled(Carousel)`
-  div:nth-child(1) {
-    margin: 0 0.6rem;
-    border-radius: 10px;
+  @media screen and (max-width: 768px) {
+    padding: 1.5rem 1.5rem 1.5rem 1rem;
   }
 `;
 
-export const FlexContainer = styled.div`
+export const TitleCard = styled.h3`
+  color: #1d164d;
+  font-family: Montserrat-Bold;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  margin-bottom: 1rem;
+`;
+
+export const ContainerCardAuthor = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const AuthorImage = styled(Image)`
+  height: 3rem;
+  width: 3rem;
+  margin-right: 1rem;
+`;
+
+export const Author = styled.span`
+  font-family: Mulish;
+  color: #9e9baf;
+`;
+
+/* Component Carousel */
+export const ContainerReadBlog = styled.section`
   display: flex;
   flex-direction: column;
-  padding: 1.5rem;
+  align-items: center;
+  background: #fafafc;
+  padding: 4rem 0 4rem 2rem;
+  max-width: 100vw;
+
+  @media screen and (max-width: 1024px) {
+    padding: 4rem 0;
+  }
+`;
+
+export const ScrollCardsBlog = styled.section`
+  width: 100%;
+  position: relative;
+  align-self: flex-end;
+  margin-top: 3rem;
+`;
+
+export const ContainerCards = styled.div`
+  display: flex;
+  flex-direction: row;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+  padding: 0 1rem 2rem;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ContainerArrow = styled.div`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  cursor: pointer;
+`;
+
+export const ArrowLeft = styled(ContainerArrow)`
+  left: 1.5rem;
+`;
+
+export const ArrowRight = styled(ContainerArrow)`
+  right: 1.5rem;
+`;
+
+export const SVGArrowLeft = styled(BsArrowLeft)`
+  width: 2.3rem;
+  height: 2.3rem;
+  background: #fff;
+`;
+
+export const SVGArrowRight = styled(BsArrowRight)`
+  width: 2.3rem;
+  height: 2.3rem;
+  background: #fff;
 `;
