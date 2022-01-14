0f2db2e9b6354f52e8bf4f50530218a3a66c6a24
Cristiano <cristianosql02@gmail.com>
2022-01-14 01:10:45 -0300

    refactor: desacoplamento

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,13 @@
 import * as S from "./styles";
-import listButton from "./content";
-import { useHistory } from "react-router-dom";
-import { Button } from "../index";
+import { Menu } from "..";
 
 const Header = () => {
-  const history = useHistory();
-  const toRegister = () => {
-    history.push("/register");
-  };
   return (
     <S.Header>
       <S.Brand>
         <S.Title>Healthy Food</S.Title>
       </S.Brand>
-      <S.Info>
-        <S.InfoContent>
-          {listButton.map(({ content, id, link }) => (
-            <S.Links href={link} key={id}>
-              {content}
-            </S.Links>
-          ))}
-          <S.DivButton>
-            <Button onClick={toRegister} content={"REGISTER"}></Button>
-          </S.DivButton>
-        </S.InfoContent>
-      </S.Info>
+      <Menu />
     </S.Header>
   );
 };
