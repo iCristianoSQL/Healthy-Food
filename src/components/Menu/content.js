b027700c57bda492fc545728cbd0e1478a140fc0
Cristiano <cristianosql02@gmail.com>
2022-01-14 01:11:16 -0300

    feat: componetizando o array

---
+++ src/components/Menu/content.js
@@ -0,0 +1,19 @@
+const listButton = [
+  {
+    id: 1,
+    content: "HEALTHY RECIPES",
+    link: "https://www.facebook.com.br",
+  },
+  {
+    id: 2,
+    content: "BLOG",
+    link: "https://www.google.com.br",
+  },
+  {
+    id: 3,
+    content: "JOIN",
+    link: "https://www.youtube.com.br",
+  },
+];
+
+export default listButton;
