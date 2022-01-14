aa7430e3e74f35257016e59d7605721ee8c116d6
Cristiano <cristianosql02@gmail.com>
2022-01-14 01:12:08 -0300

    feat: desacoplando a logica do yup

---
+++ src/pages/Register/registerSchema.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+export const registerSchema = Yup.object().shape({
+  name: Yup.string().required(""),
+  cpf: Yup.string().required(""),
+  cep: Yup.string()
+    .min(8, "This field must have eight digits")
+    .max(8, "This field must have eight digits")
+    .required(""),
+  address: Yup.string().required(""),
+  district: Yup.string().required(""),
+  location: Yup.string().required(""),
+  uf: Yup.string()
+    .min(2, "This field must have two digits")
+    .max(2, "This field must have two digits")
+    .required(""),
+});
