50c50096dbaa681d599d66f20b28482b5a1caa29
Cristiano <cristianosql02@gmail.com>
2022-01-18 03:57:29 -0300

    refactor: melhorando a validação

--- src/pages/Register/registerSchema.js
+++ src/pages/Register/registerSchema.js
@@ -7,16 +7,16 @@ export const registerSchema = Yup.object().shape({
     .max(new Date(), "The deadline is the current day")
     .required(""),
   cpf: Yup.string()
-    .min(11, "This field must have eleven digits")
+    .min(14, "This field must have eleven digits")
     .max(14, "This field must have eleven digits")
     .required(""),
   cep: Yup.string()
-    .min(8, "This field must have eight digits")
+    .min(9, "This field must have eight digits")
     .max(9, "This field must have eight digits")
     .required(""),
-  address: Yup.string().required(""),
-  district: Yup.string().required(""),
-  location: Yup.string().required(""),
+  address: Yup.string().min(3, "Enter a valid address").required(""),
+  district: Yup.string().min(3, "Enter a valid neighborhood").required(""),
+  location: Yup.string().min(3, "Enter a valid location").required(""),
   uf: Yup.string()
     .min(2, "This field must have two digits")
     .max(2, "This field must have two digits")
